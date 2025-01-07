27679e6918d9624be9d6909690711e9d5b79265e
Ian Oliver <ian.justin.oliver@gmail.com>
2025-01-07 13:10:56 +0200

    updated shellscripts to work with the new schemas

--- scripts/shellscriptideas/att.sh
+++ scripts/shellscriptideas/att.sh
@@ -1,24 +1,32 @@
 #!/bin/sh
 
-ELEMENT=765e0188-a609-49c0-bf61-3825849413cd
-POLICY=ba64f197-0eb3-4f3f-a74d-a9a8e0a3f17d
+ELEMENT=0655caed-65ce-4755-a951-9f37e81904e2
+POLICY1=std::intent::sys::info
+POLICY2=std::intent::sha256::crtm::pcr0
+POLICY3=std::intent::linux::ima::asciilog
 
-EN="$(curl -s -X GET http://127.0.0.1:8520/element/$ELEMENT | jq -r .name)"
-PN="$(curl -s -X GET http://127.0.0.1:8520/policy/$POLICY | jq -r .name)"
+
+
+EN="$(curl -s -X GET http://192.168.0.40:8520/element/$ELEMENT | jq -r .name)"
+PN="$(curl -s -X GET http://192.168.0.40:8520/intent/$POLICY | jq -r .name)"
 
 
 echo Applying $PN to $EN
 
 
 #open session
-SESSION="$(curl -s -X POST http://127.0.0.1:8520/session -H "Content-Type: application/json" --data '{"message":"test from curl"}' | jq -r .itemid)"
+SESSION="$(curl -s -X POST http://192.168.0.40:8520/session -H "Content-Type: application/json" --data '{"message":"test from curl"}' | jq -r .itemid)"
 
 #attest element
-CLAIMID="$(curl -s -X POST http://127.0.0.1:8520/attest -H  "Content-Type: application/json" --data '{"eid":"'$ELEMENT'","pid":"'$POLICY'","sid":"'$SESSION'"}' | jq -r .itemid)"
+echo $ELEMENTID
+
+CLAIMID1="$(curl -s -X POST http://192.168.0.40:8520/attest -H  "Content-Type: application/json" --data '{"eid":"'$ELEMENT'","epn":"tarzan", "pid":"'$POLICY1'","sid":"'$SESSION'"}' | jq -r .itemid)"
+CLAIMID2="$(curl -s -X POST http://192.168.0.40:8520/attest -H  "Content-Type: application/json" --data '{"eid":"'$ELEMENT'","epn":"tarzan", "pid":"'$POLICY2'","sid":"'$SESSION'"}' | jq -r .itemid)"
+CLAIMID3="$(curl -s -X POST http://192.168.0.40:8520/attest -H  "Content-Type: application/json" --data '{"eid":"'$ELEMENT'","epn":"tarzan", "pid":"'$POLICY3'","sid":"'$SESSION'"}' | jq -r .itemid)"
 
 #close session
-curl -s -X DELETE http://127.0.0.1:8520/session/$SESSION > /dev/null
+curl -s -X DELETE http://192.168.0.40:8520/session/$SESSION > /dev/null
 
 #Print stuff
-echo Session was $SESSION with claim $CLAIMID
-curl -s -X GET http://127.0.0.1:8520/claim/$CLAIMID | jq .body.sha256
+echo Session was $SESSION with claim obtained is $CLAIMID
+curl -s -X GET http://192.168.0.40:8520/session/$SESSION | jq .
